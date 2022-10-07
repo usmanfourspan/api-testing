290c331923898c380c246c00eeb621213a1c9246
Usman Arshad <usman.arshad@fourspan.com>
2022-10-07 10:48:01 +0500

    Add the project owner column and make it foreign key in the project table
    use latest syntax for it

--- database/migrations/2022_10_04_063931_create_projects_table.php
+++ database/migrations/2022_10_04_063931_create_projects_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -12,6 +13,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->uuid();
+            $table->foreignIdFor(User::class);
             $table->string('title');
             $table->text('description');
             $table->timestamps();
